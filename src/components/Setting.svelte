e564845d61f890d0b62b31ffbf485501bae29bf6
ovaphlow <ovaphlow@ROG-G533QR.>
2024-04-25 15:46:41 +0800

    setting

--- src/components/Setting.svelte
+++ src/components/Setting.svelte
@@ -1,6 +1,33 @@
 <script>
+    import axios from "axios";
+    import { CRATE_API_HOST } from "../constants.mjs";
+
     export let mode = "list";
     export let settings = [];
+
+    let id = "";
+    let k = "";
+    let v = "";
+
+    async function submit() {
+        if (mode === "update") {
+            const s = settings.find((it) => it._id === id);
+            if (!s) return;
+            const uuid = JSON.parse(s.state)?.uuid;
+            const response = await axios.put(
+                CRATE_API_HOST + `/crate/setting/${uuid}/${id}`,
+                {
+                    detail: JSON.stringify({ name: k, value: v }),
+                },
+            );
+            if (response.status >= 400) {
+                console.error(response);
+                return;
+            }
+            location.refresh(true);
+            mode = "list";
+        }
+    }
 </script>
 
 <div class="card w-full bg-base-200 shadow">
@@ -40,7 +67,16 @@
                                 <th class="text-center">
                                     <button
                                         class="btn btn-info btn-outline btn-xs"
-                                        >编辑</button
+                                        on:click={() => {
+                                            id = setting._id;
+                                            k =
+                                                JSON.parse(setting?.detail)
+                                                    ?.name || "";
+                                            v =
+                                                JSON.parse(setting?.detail)
+                                                    ?.value || "";
+                                            mode = "update";
+                                        }}>编辑</button
                                     >
                                 </th>
                             </tr>
@@ -60,15 +96,24 @@
                 </table>
             </div>
         {:else if mode === "create" || mode === "update"}
-            <button
-                class="btn btn-secondary btn-sm"
-                on:click={() => {
-                    mode = "list";
-                }}>返回</button
-            >
-            <form>
-                <div class="card-actions justify-end">
-                    <button class="btn btn-primary">Buy Now</button>
+            <div class="flex flex-row"></div>
+            <form on:submit|preventDefault={submit} class="flex flex-col gap-4">
+                <label class="input input-bordered flex items-center gap-2">
+                    名称
+                    <input type="text" bind:value={k} class="grow" />
+                </label>
+                <label class="input input-bordered flex items-center gap-2">
+                    值
+                    <input type="text" bind:value={v} class="grow" />
+                </label>
+                <div class="card-actions justify-between">
+                    <button
+                        class="btn btn-secondary btn-sm"
+                        on:click={() => (mode = "list")}>返回</button
+                    >
+                    <button type="submit" class="btn btn-primary btn-sm"
+                        >提交</button
+                    >
                 </div>
             </form>
         {/if}
