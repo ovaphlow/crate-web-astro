80df2e483f639647eadc4925aad70a862338ba8c
ovaphlow <ovaphlow@live.com>
2024-05-14 10:14:07 +0800

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
+import alpinejs from "@astrojs/alpinejs";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
+    integrations: [tailwind(), svelte(), alpinejs()],
+    output: "server",
+    adapter: node({
+        mode: "standalone"
+    })
 });
